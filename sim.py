559acd471fc4f48131d56edef326e0daecf67bac
Carl Smestad <carl.smestad@gmail.com>
2023-03-29 10:52:52 +0200

    working timeout chances and window

--- sim.py
+++ sim.py
@@ -1,6 +1,9 @@
 import os
+import time
 import math
+import random
 from typing import Tuple, List
+import matplotlib.pyplot as plt
 
 from flwr.common import Metrics
 
@@ -10,8 +13,11 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import flwr as fl
 import tensorflow as tf
 
-
 NUM_CLIENTS = 100
+TIMEOUT_WINDOW = 10
+TIMEOUT_CHANCE = 0.05
+SHOULD_TIMEOUT = False
+
 
 
 class FlwrClient(fl.client.NumPyClient):
@@ -21,11 +27,16 @@ class FlwrClient(fl.client.NumPyClient):
         split_idx = math.floor(len(x_train) * 0.9)  # Use 10% of x_train for validation
         self.x_train, self.y_train = x_train[:split_idx], y_train[:split_idx]
         self.x_val, self.y_val = x_train[split_idx:], y_train[split_idx:]
+        self.should_timeout = (random.random() < TIMEOUT_CHANCE) and SHOULD_TIMEOUT
 
     def get_parameters(self, config):
         return self.model.get_weights()
 
     def fit(self, parameters, config):
+
+        if self.should_timeout:
+            time.sleep(TIMEOUT_WINDOW+1)
+
         self.model.set_weights(parameters)
         self.model.fit(self.x_train, self.y_train, epochs=2, verbose=2)
         return self.model.get_weights(), len(self.x_train), {}
@@ -64,25 +75,52 @@ def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
 
     return {"accuracy": sum(accuracies) / sum(examples)}
 
+def fit_config(server_round: int):
+    """Return training configuration dict for each round.
+
+    Perform two rounds of training with one local epoch, increase to two local
+    epochs afterwards.
+    """
+    config = {
+        "server_round": server_round,  # The current round of federated learning
+        "local_epochs": 1
+    }
+    return config
+
 def main() -> None:
     # Start Flower simulation
     result = fl.simulation.start_simulation(
+        ray_init_args={"include_dashboard": False},
         client_fn=client_fn,
         num_clients=NUM_CLIENTS,
         client_resources={"num_cpus": 4},
-        config=fl.server.ServerConfig(num_rounds=1),
+        config=fl.server.ServerConfig(num_rounds=10, round_timeout=TIMEOUT_WINDOW),
         strategy=fl.server.strategy.FedAvg(
             fraction_fit=0.1,
             fraction_evaluate=0.1,
-            min_fit_clients=10,
-            min_evaluate_clients=10,
-            min_available_clients=NUM_CLIENTS,
+            min_fit_clients=3,
+            min_evaluate_clients=2,
+            min_available_clients=10,
             evaluate_metrics_aggregation_fn=weighted_average,
+            on_fit_config_fn=fit_config,
         ),
     )
 
-    print(result.metrics_distributed)
+    plt.plot(*zip(*result.metrics_distributed['accuracy']))
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['Train'], loc='upper left')
+    plt.show()
+    """ 
 
+    plt.plot(*zip(*result.losses_distributed))
+    plt.title('model losses')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['Loss'], loc='upper left')
+    plt.show()
+"""
 
 if __name__ == "__main__":
     main()
