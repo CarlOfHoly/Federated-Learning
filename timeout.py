448bd61d5b1ed70253ea42cc35d78221298931eb
Carl Smestad <carl.smestad@gmail.com>
2023-03-30 07:34:28 +0200

    add file for testing dynamic timeout window

--- timeout.py
+++ timeout.py
@@ -1,4 +1,5 @@
 import os
+import timeit
 import time
 import math
 import random
@@ -11,6 +12,7 @@ from flwr.common.logger import log
 from flwr.common import Metrics
 from flwr.server.client_proxy import ClientProxy
 from flwr.server.criterion import Criterion
+from flwr.server.history import History
 
 # Make TensorFlow logs less verbose
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
@@ -19,9 +21,9 @@ import flwr as fl
 import tensorflow as tf
 
 NUM_CLIENTS = 100
-TIMEOUT_WINDOW = 10
-TIMEOUT_CHANCE = 0.05
-SHOULD_TIMEOUT = False
+TIMEOUT_WINDOW = 5
+TIMEOUT_CHANCE = 0.6
+SHOULD_TIMEOUT = True
 
 
 class FlwrClient(fl.client.NumPyClient):
@@ -38,7 +40,7 @@ class FlwrClient(fl.client.NumPyClient):
 
     def fit(self, parameters, config):
         if self.should_timeout:
-            time.sleep(TIMEOUT_WINDOW + 1)
+            time.sleep(TIMEOUT_WINDOW + 2)
 
         self.model.set_weights(parameters)
         self.model.fit(self.x_train, self.y_train, epochs=2, verbose=2)
@@ -137,8 +139,83 @@ class ClientManager(fl.server.client_manager.ClientManager, ABC):
 
 class CustomServer(fl.server.Server):
     def __init__(self):
-        super().__init__(client_manager=CustomClientManager())
+        super().__init__(client_manager=CustomClientManager(),
+                         strategy=fl.server.strategy.FedAvg(
+                             fraction_fit=0.1,
+                             fraction_evaluate=0.1,
+                             min_fit_clients=3,
+                             min_evaluate_clients=2,
+                             min_available_clients=10,
+                             evaluate_metrics_aggregation_fn=weighted_average,
+                             on_fit_config_fn=fit_config,
+                         )),
 
+    # pylint: disable=too-many-locals
+    def fit(self, num_rounds: int, timeout: Optional[float]) -> History:
+        """Run federated averaging for a number of rounds."""
+        history = History()
+
+        # Initialize parameters
+        log(INFO, "Initializing global parameters CUSTOM SERVER")
+        self.parameters = self._get_initial_parameters(timeout=timeout)
+        log(INFO, "Evaluating initial parameters CUSTOM SERVER")
+        res = self.strategy.evaluate(0, parameters=self.parameters)
+        if res is not None:
+            log(
+                INFO,
+                "initial parameters (loss, other metrics): %s, %s",
+                res[0],
+                res[1],
+            )
+            history.add_loss_centralized(server_round=0, loss=res[0])
+            history.add_metrics_centralized(server_round=0, metrics=res[1])
+
+        # Run federated learning for num_rounds
+        log(INFO, "FL starting")
+        start_time = timeit.default_timer()
+
+        for current_round in range(1, num_rounds + 1):
+            # Train model and replace previous global model
+            res_fit = self.fit_round(server_round=current_round, timeout=timeout)
+            if res_fit:
+                parameters_prime, _, _ = res_fit  # fit_metrics_aggregated
+                if parameters_prime:
+                    self.parameters = parameters_prime
+
+            # Evaluate model using strategy implementation
+            res_cen = self.strategy.evaluate(current_round, parameters=self.parameters)
+            if res_cen is not None:
+                loss_cen, metrics_cen = res_cen
+                log(
+                    INFO,
+                    "fit progress: (%s, %s, %s, %s)",
+                    current_round,
+                    loss_cen,
+                    metrics_cen,
+                    timeit.default_timer() - start_time,
+                    )
+                history.add_loss_centralized(server_round=current_round, loss=loss_cen)
+                history.add_metrics_centralized(
+                    server_round=current_round, metrics=metrics_cen
+                )
+
+            # Evaluate model on a sample of available clients
+            res_fed = self.evaluate_round(server_round=current_round, timeout=timeout)
+            if res_fed:
+                loss_fed, evaluate_metrics_fed, _ = res_fed
+                if loss_fed:
+                    history.add_loss_distributed(
+                        server_round=current_round, loss=loss_fed
+                    )
+                    history.add_metrics_distributed(
+                        server_round=current_round, metrics=evaluate_metrics_fed
+                    )
+
+        # Bookkeeping
+        end_time = timeit.default_timer()
+        elapsed = end_time - start_time
+        log(INFO, "FL finished in %s", elapsed)
+        return history
 
 def main() -> None:
     # Start Flower simulation
@@ -147,18 +224,9 @@ def main() -> None:
         client_fn=client_fn,
         num_clients=NUM_CLIENTS,
         client_resources={"num_cpus": 4},
-        config=fl.server.ServerConfig(num_rounds=3, round_timeout=TIMEOUT_WINDOW),
+        config=fl.server.ServerConfig(num_rounds=5, round_timeout=TIMEOUT_WINDOW),
         client_manager=CustomClientManager(),
         server=CustomServer(),
-        strategy=fl.server.strategy.FedAvg(
-            fraction_fit=0.1,
-            fraction_evaluate=0.1,
-            min_fit_clients=3,
-            min_evaluate_clients=2,
-            min_available_clients=10,
-            evaluate_metrics_aggregation_fn=weighted_average,
-            on_fit_config_fn=fit_config,
-        ),
     )
 
     plt.plot(*zip(*result.metrics_distributed['accuracy']))
